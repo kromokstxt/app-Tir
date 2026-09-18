3129f432112daf3a488aa73d54d32326f8e24bc4
kromoks.txt <thomkevlar@gmail.com>
2026-09-18 21:15:22 +0200

    Move HelloContrloller to mains source, add Spring Boot project

---
+++ demo/src/main/java/com/Tir/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.Tir.demo;
+
+import  org.springframework.web.bind.annotation.GetMapping;
+import  org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HelloController{
+
+    @GetMapping("/")
+    public String home () {
+        return "Hello tireur";
+
+    }
+}
+
