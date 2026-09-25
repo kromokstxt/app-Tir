ef0214b19929d5ea541c368449b583d9534f03aa
kromoks.txt <thomkevlar@gmail.com>
2026-09-25 13:39:25 +0200

    Ajout de la classe Shooter et ShooterController

---
+++ demo/src/main/java/com/Tir/demo/Shooter.java
@@ -0,0 +1,26 @@
+package com.Tir.demo;
+
+public class Shooter {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Shooter(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
